49dceeba824659a60966b24193e5d46e2e2406fe
mucr <mucr@liandisys.com.cn>
2022-08-24 17:50:06 +0800

    Set config file

--- syncFoldersColorTags/main.py
+++ syncFoldersColorTags/main.py
@@ -8,5 +8,5 @@ referredFolder = input("来源文件夹路径：")
 targetFolder = input("目标文件夹路径：")
 referredFolderInfo = os.popen("cd \"" + referredFolder + "\" && tag")
 for folder in referredFolderInfo.readlines():
-    colorInfo = folder.split("\t")[len(folder.split("\t")) - 1]
-    subprocess.call(['tag', '-s', colorInfo.split("\n")[0], targetFolder + "/" + folder.split(" ")[0]])
+    colorInfo = (folder.split("\t")[len(folder.split("\t")) - 1]).split("\n")[0]
+    subprocess.call(['tag', '-s', colorInfo, targetFolder + "/" + folder.split(" ")[0]])
